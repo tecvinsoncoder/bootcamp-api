d760564d3aa6ca681650044d57187e3e5f53a695
Oladejo Adesola <adesola@tecvinson.com>
2023-11-26 13:53:14 +0000

    Added the participant class

---
+++ Entities/Participant.cs
@@ -0,0 +1,10 @@
+namespace Demo.Entities
+{
+    public class Participant
+    {
+        public Guid Id {set; get;}
+        public string? FirstName {set; get;}
+        public string? LastName {set; get;}
+        public string? Email { set; get;}
+    }
+}
